2f14826f232c1e31c2f2103309737d35fda6aac8
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-25 15:46:54 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Nome Completo ]
+
+String Nome;
+String Sobrenome;
+
+Console.Write("Digite seu Nome:");
+Nome = Console.ReadLine()!;
+
+Console.Write("Digite seu Sobrenome: ");
+Sobrenome = Console.ReadLine()!;
+Console.WriteLine();
+
+Console.WriteLine($"Nome Completo: {Nome} {Sobrenome}");
+
+Console.WriteLine($"Nome Catálogo: {Sobrenome.ToUpper()}, {Nome}");
